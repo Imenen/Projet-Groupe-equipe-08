873ec45d5023dfbe96ff0fb0de85016b7bf36bd2
Imenen <elouchdiimene@gmail.com>
2023-02-26 19:29:14 +0100

    Create ProductNotification.java

---
+++ App Gestion De Stock De Peinture/App Gestion De Stock De Peinture/src/ProductNotification.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List ;
+
+	
+
+public class ProductNotification {
+    private Product product;
+    private int minimumQuantity;
+
+    public ProductNotification(Product product, int minimumQuantity) {
+        this.product = product;
+        this.minimumQuantity = minimumQuantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getMinimumQuantity() {
+        return minimumQuantity;
+    }
+
+    public boolean shouldNotify(int currentQuantity) {
+        return currentQuantity <= minimumQuantity;
+    }
+}
